b6f3868dd3751163e3b0c20353645aa54af7e1e4
Miguel <fagundezdev@gmail.com>
2020-08-23 09:53:31 -0400

    Users Permissions UI modification

--- app/src/main/java/com/devproject/miguelfagundez/wallpaperexample/MainActivity.kt
+++ app/src/main/java/com/devproject/miguelfagundez/wallpaperexample/MainActivity.kt
@@ -3,7 +3,13 @@ package com.devproject.miguelfagundez.wallpaperexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
+
+    // Members
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
